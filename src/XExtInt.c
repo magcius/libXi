6585b076f6d94281058071f159b5bd8aba694e57
Jasper St. Pierre <jstpierre@mecheye.net>
2012-11-30 18:39:07 -0500

    Reverting "remove x/y fields" Revert "remove x/y fields"
    This reverts commit 692f7193e615da04a9e59dda3f4b739960a55908.

--- src/XExtInt.c
+++ src/XExtInt.c
@@ -2006,6 +2006,9 @@ wireToBarrierEvent(xXIBarrierEvent *in, XGenericEventCookie *cookie)
     out->extension = in->extension;
     out->evtype = in->evtype;
 
+    out->x = in->x;
+    out->y = in->y;
+
     out->dx = FP3232_TO_DOUBLE (in->dx);
     out->dy = FP3232_TO_DOUBLE (in->dy);
     out->raw_dx = FP3232_TO_DOUBLE (in->raw_dx);
